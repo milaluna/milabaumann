4a2506e49538d6cfd60c91769b488e753194e45b
nikensss <rikitzzz@gmail.com>
2019-12-05 22:34:33 +0100

    Added top navbar in mobile, effect of hidding and coming back when you scroll down and up. Changed the style a lot: completely removed the grid, using flexbox more often.

---
+++ js/index.js
@@ -0,0 +1,49 @@
+let didScroll;
+var lastScrollTop = 0;
+var delta = 2;
+var navbarHeight = $('#menu')
+  .outerHeight();
+
+
+// on scroll, let the interval function know the user has scrolled
+$(window)
+  .scroll(function(event) {
+    didScroll = true;
+  });
+
+// run hasScrolled() and reset didScroll status
+setInterval(function() {
+  if (didScroll) {
+    hasScrolled();
+    didScroll = false;
+  }
+}, 250);
+
+function hasScrolled() {
+  var st = $(this)
+    .scrollTop();
+
+  if (Math.abs(lastScrollTop - st) <= delta) {
+    return;
+  }
+
+  // If current position > last position AND scrolled past navbar...
+  if (st > lastScrollTop && st > navbarHeight) {
+    // Scroll Down
+    $('#menu')
+      .removeClass('menu-show')
+      .addClass('menu-hide');
+  } else {
+    // Scroll Up
+    // If did not scroll past the document (possible on mac)...
+    if (st + $(window)
+      .height() < $(document)
+      .height()) {
+      $('#menu')
+        .removeClass('menu-hide')
+        .addClass('menu-show');
+    }
+  }
+
+  lastScrollTop = st;
+}
