f405bb0f04863e9c75e0a89e93f8bb6d5ed95f1e
nikensss <rikitzzz@gmail.com>
2020-05-26 23:56:59 +0200

    fixed bug when turning background back to white: the problem was that the offsets had to be calculated on every scroll event

--- flowState/js/index.js
+++ flowState/js/index.js
@@ -1,27 +1,17 @@
 $(document).ready(function () {
-  const goalOffset = $('#goal-title').offset().top;
-  const movieViewerOffset = $('#movies').offset().top;
-  const movieCarousselOffset = $('#veryuniqueid').offset().top;
-  const movieCarousselHeight = $('#veryuniqueid').height();
   $(document).scroll(function () {
+    const goalOffset = $('#goal-title').offset().top;
+    const movieViewerOffset = $('#movies').offset().top;
+    const movieCarousselOffset = $('#movie-caroussel').offset().top;
+    const movieCarousselHeight = $('#movie-caroussel').height();
     const pos = $(this).scrollTop();
     let newBg = 255;
-    if (pos >= goalOffset && pos < movieViewerOffset) {
-      console.log('going black');
+    if (pos >= goalOffset && pos <= movieViewerOffset) {
       newBg = 255 - ((pos - goalOffset) / (movieViewerOffset - goalOffset)) * 255;
     } else if (pos > movieViewerOffset && pos < movieCarousselOffset) {
-      console.log('staying black');
       newBg = 0;
     } else if (pos >= movieCarousselOffset && pos < movieCarousselOffset + movieCarousselHeight) {
-      console.log('going white');
-      console.log(
-        `(${pos}-${movieCarousselOffset})/${movieCarousselHeight} = ${
-          (pos - movieCarousselOffset) / movieCarousselHeight
-        }`
-      );
       newBg = ((pos - movieCarousselOffset) / movieCarousselHeight) * 255;
-    } else if (pos >= movieCarousselOffset + movieCarousselHeight) {
-      newBg = 255;
     }
     $(document.body).css('background-color', `rgb(${newBg},${newBg},${newBg})`);
   });
