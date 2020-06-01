e6b90893ce3c5e7fb64ceca7092fadc97baf03c4
nikensss <rikitzzz@gmail.com>
2020-06-01 16:46:51 +0200

    color transitions done properly

--- flowState/js/index.js
+++ flowState/js/index.js
@@ -1,21 +1,23 @@
 $(document).ready(() => {
   $(document).scroll(() => {
     const goalOffset = $('#goal-title').offset().top;
-    const movieViewerOffset = $('#movies').offset().top;
+    const moviesOffset = $('#movies').offset().top;
     const movieCarousselOffset = $('#movie-caroussel').offset().top;
-    const movieCarousselHeight = $('#movie-caroussel').height();
+    const collectionOfMovementsOffset = $('#collection-of-movements').offset().top;
     const pos = $(document).scrollTop();
     let newBg = 255;
-    if (pos >= goalOffset && pos <= movieViewerOffset) {
-      newBg = 255 - ((pos - goalOffset) / (movieViewerOffset - goalOffset)) * 255;
-    } else if (pos > movieViewerOffset && pos < movieCarousselOffset) {
+    if (pos >= goalOffset && pos <= moviesOffset) {
+      newBg = 255 - ((pos - goalOffset) / (moviesOffset - goalOffset)) * 255;
+    } else if (pos > moviesOffset && pos < movieCarousselOffset) {
       newBg = 0;
-    } else if (pos >= movieCarousselOffset && pos < movieCarousselOffset + movieCarousselHeight) {
-      newBg = ((pos - movieCarousselOffset) / movieCarousselHeight) * 255;
+    } else if (pos >= movieCarousselOffset && pos < collectionOfMovementsOffset) {
+      newBg = ((pos - movieCarousselOffset) / (collectionOfMovementsOffset - movieCarousselOffset)) * 255;
+      $('.com-title').css('opacity', newBg / 255); //code is shorted, but we just need a number that goes from 0 to 1
     }
-    $('body').css('background-color', `rgb(${newBg},${newBg},${newBg})`);
+    $('.bg-color-change').css('background-color', `rgb(${newBg},${newBg},${newBg})`);
   });
 
+  // initilises the coursel with the slick library
   $('.movie-caroussel').slick({
     infinite: true,
     slidesToShow: 3,
