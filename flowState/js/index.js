cd068deb3cd9ad836ad5f077426d85fda406ebe5
Ricard Mallafré López <rikitzzz@gmail.com>
2020-06-14 15:33:20 +0200

    added movements in the collection of movements
    Added movements, their corresponding timeline and the video of the
    movement. Also, created the JS necessary to link a timeline to a
    video. The time indicator in the timeline is done with CSS and
    controlled through jQuery and the Vimeo API.

--- flowState/js/index.js
+++ flowState/js/index.js
@@ -1,4 +1,15 @@
 $(document).ready(() => {
+  // initilises the coursel with the slick library
+  $('.movie-caroussel').slick({
+    infinite: true,
+    slidesToShow: 3,
+    autoplay: true,
+    autoplaySpeed: 1200,
+    arrows: false
+  });
+
+  //effect to smootly transition screen to black when arriving to #movies
+  //and then back to white when leaving #movies
   $(document).scroll(() => {
     const goalOffset = $('#goal-title').offset().top;
     const moviesOffset = $('#movies').offset().top;
@@ -17,12 +28,37 @@ $(document).ready(() => {
     $('.bg-color-change').css('background-color', `rgb(${newBg},${newBg},${newBg})`);
   });
 
-  // initilises the coursel with the slick library
-  $('.movie-caroussel').slick({
-    infinite: true,
-    slidesToShow: 3,
-    autoplay: true,
-    autoplaySpeed: 1200,
-    arrows: false
-  });
+  //sync vimeo video play with timeline animation
+  const flyinghHigherAndHigher = new TimeLinePlayer('flying-higher-and-higher');
+  const exchange = new TimeLinePlayer('exchange');
 });
+
+class TimeLinePlayer {
+  constructor(target) {
+    if (!target) throw new Error('Invalid class!', target);
+    this.target = $(`#${target}>.timeline-display>.timeline-container>.time`);
+    this.player = new Vimeo.Player(document.querySelector(`#${target}>.timeline-video>iframe`));
+    this.link();
+  }
+
+  link() {
+    this.player.on('play', (event) => {
+      console.log('played the video!', event);
+      console.log('duration', event.duration);
+      if (this.target.css('animation-name') === 'timeline-animation') {
+        this.target.css('animation-play-state', 'running');
+      } else {
+        this.target.css('animation-name', 'timeline-animation');
+        this.target.css('animation-duration', `${event.duration}s`);
+        this.target.css('animation-timing-function', 'linear');
+        this.target.css('animation-play-state', 'running');
+      }
+    });
+
+    this.player.on('pause', () => this.target.css('animation-play-state', 'paused'));
+
+    this.player.on('ended', () => this.target.css('animation-name', 'none'));
+
+    this.player.getVideoTitle().then((title) => console.log('title:', title));
+  }
+}
